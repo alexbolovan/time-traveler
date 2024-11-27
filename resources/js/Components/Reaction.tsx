7428372b775ed08434cf8e47be80d3324d046c33
Alexander Bolovan <alexbolovan@gmail.com>
2024-11-26 22:00:18 -0500

    added bubble component for abstraction on reaction

--- resources/js/Components/Reaction.tsx
+++ resources/js/Components/Reaction.tsx
@@ -1,24 +1,44 @@
 import {PageProps} from "@/types";
 import {useState} from "react";
 
-export default function Reaction({like_count, dislike_count, amazed_count, clown_count} : PageProps<{like_count : number, dislike_count : number, amazed_count : number, clown_count : number}>) {
+function Bubble({children}: PageProps) {
+    return (
+        <span
+            className="inline-flex items-center gap-x-2 rounded-full px-4 py-2 text-sm font-medium text-white ring-2 ring-inset ring-gray-200"
+            onClick={(e) => {
+                console.log("Like")
+                e.stopPropagation();
+            }}>
+            {children}
+    </span>
+
+    );
+
+}
+
+
+export default function Reaction({like_count, dislike_count, amazed_count, clown_count}: PageProps<{
+    like_count: number,
+    dislike_count: number,
+    amazed_count: number,
+    clown_count: number
+}>) {
 
 
     return (
         <div className="flex ml-6 space-x-4 pb-2">
-            <span
-                className="inline-flex items-center gap-x-2 rounded-full px-4 py-2 text-sm font-medium text-white ring-2 ring-inset ring-gray-200">
+            <Bubble>
                 👍 {like_count}
-            </span>
-            <span className="inline-flex items-center gap-x-2 rounded-full px-4 py-2 text-sm font-medium text-white ring-2 ring-inset ring-gray-200">
+            </Bubble>
+            <Bubble>
                 👎 {dislike_count}
-            </span>
-            <span className="inline-flex items-center gap-x-2 rounded-full px-4 py-2 text-sm font-medium text-white ring-2 ring-inset ring-gray-200">
+            </Bubble>
+            <Bubble>
                 🤯 {amazed_count}
-            </span>
-            <span className="inline-flex items-center gap-x-2 rounded-full px-4 py-2 text-sm font-medium text-white ring-2 ring-inset ring-gray-200">
+            </Bubble>
+            <Bubble>
                 🤡 {clown_count}
-            </span>
+            </Bubble>
         </div>
     );
 }
