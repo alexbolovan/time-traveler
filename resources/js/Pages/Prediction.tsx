878cfee8447e15fe9fa573aa9ccaea8d72067361
Alexander Bolovan <alexbolovan@gmail.com>
2024-11-29 19:00:42 -0500

    designed post info on predicition page

--- resources/js/Pages/Prediction.tsx
+++ resources/js/Pages/Prediction.tsx
@@ -1,24 +1,36 @@
 import {PageProps} from "@/types";
 import predictions from "@/Pages/Predictions";
+import Tags from "@/Components/Tags";
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+import Guest from "@/Layouts/GuestLayout";
 
 export default function Prediction({auth, prediction, id}: PageProps<{ auth: boolean, prediction: any, id: number }>) {
+    const RenderedComponent = auth ? Authenticated : Guest;
 
     return (
-        <>
-            <div className="flex-col space-y-2 pb-6">
-                <h1><pre>Title </pre> {prediction.title}</h1>
-                <p><pre>Body </pre> {prediction.body}</p>
+        <RenderedComponent>
+            {/* Prediction content*/}
+            <div className="overflow-hidden rounded-lg bg-transparent shadow border border-white pt-3 pb-1">
+                <Tags/>
+                <div className="px-4 sm:px-6">
+                    <p className="text-2xl mb-1">{prediction.title}</p>
+                    <p className="text-sm hover:underline">{prediction.user.name}</p>
+                </div>
+                <div className="px-4 pb-2 sm:p-6">{prediction.body}</div>
             </div>
 
-            <div className="flex-col space-y-2 pb-6">
-                <h2 className="underline">Comments</h2>
-                <ul>
-                    {prediction.comments.map((comment: string, index: number) => (
-                        <div className="flex-col space-y-4">
-                            {!comment.parent_id ?
-                                <li key={index}>
-                                    <p>Comment: {comment.body}</p>
-                                    {comment.comments.map((subcomment: string, index: number) => (
+            {/* Filter options */}
+
+
+                <div className="flex-col space-y-2 pb-6">
+                    <h2 className="underline">Comments</h2>
+                    <ul>
+                        {prediction.comments.map((comment: string, index: number) => (
+                            <div className="flex-col space-y-4">
+                                {!comment.parent_id ?
+                                    <li key={index}>
+                                        <p>Comment: {comment.body}</p>
+                                        {comment.comments.map((subcomment: string, index: number) => (
                                         <p>Subcomment: {subcomment.body}</p>
                                     ))}
                                 </li>
@@ -29,6 +41,8 @@ export default function Prediction({auth, prediction, id}: PageProps<{ auth: boo
                     ))}
                 </ul>
             </div>
-        </>
+        </RenderedComponent>
     )
 }
+
+
